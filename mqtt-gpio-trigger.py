58fb817fa37bcc38278b1e4ada00734fbd56ea80
Kyle Gordon <kyle@lodge.glasgownet.com>
2013-12-30 15:26:18 +0000

    Mosquitto v1+ callbacks

--- mqtt-gpio-trigger.py
+++ mqtt-gpio-trigger.py
@@ -81,7 +81,7 @@ def connect():
 
     mqttc.subscribe(MQTT_TOPIC, 2)
 
-def on_connect(result_code):
+def on_connect(mosq, obj, result_code):
      """
      Handle connections (or failures) to the broker.
      """
@@ -93,7 +93,7 @@ def on_connect(result_code):
         logging.warning("Something went wrong")
         cleanup()
 
-def on_disconnect(result_code):
+def on_disconnect(mosq, obj, result_code):
      """
      Handle disconnections from the broker
      """
@@ -106,7 +106,7 @@ def on_disconnect(result_code):
         connect()
         main_loop()
 
-def on_message(msg):
+def on_message(mosq, obj, msg):
     """
     What to do when the client recieves a message from the broker
     """
